a94a46d3a4ad1c7669f8ce30a56478acdfab4c14
Tao <frostwolfer@gmail.com>
2018-10-28 17:40:43 -0400

    first commit

---
+++ SamuraiApp.Domain/SamuraiBattle.cs
@@ -0,0 +1,12 @@
+﻿namespace SamuraiApp.Domain
+{
+    public class SamuraiBattle
+    {
+        public int SamuraiId { get; set; }
+        public int BattleId { get; set; }
+
+        //Navigational Properties:
+        public Samurai Samurai { get; set; }
+        public Battle Battle { get; set; }
+    }
+}
---
+++ Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4e2f7cba-1c2a-437f-a717-fe939d6dabc6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
